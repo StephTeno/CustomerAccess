a0e53958e3652e3a0e0cca85caf12ebdc5a31d99
StephTeno <106124803+StephTeno@users.noreply.github.com>
2023-05-08 20:21:55 -0600

    Connection Ado Sql Success

--- WinAdo/frmAdo.cs
+++ WinAdo/frmAdo.cs
@@ -1,15 +1,23 @@
 using System.Configuration;
+using System.Data;
 using System.Runtime.InteropServices;
-using VisualUI;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WinAdo
 {
     public partial class frmAdo : Form
     {
-        string connectionString = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;
+        string connectionString = ConfigurationManager.ConnectionStrings["WinAdo.Properties.Settings.Connection"].ConnectionString;
         public frmAdo()
         {
             InitializeComponent();
+            GetClients();
         }
         //[ Esto es para que se pueda mover nuestro formulario
         [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
@@ -31,6 +39,28 @@ namespace WinAdo
         private void btnMinimizar_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+        private void GetClients()
+        {
+            string queryString = "SELECT CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax " +
+                                 "FROM Customers ORDER BY CompanyName DESC;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dgvDatos.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
